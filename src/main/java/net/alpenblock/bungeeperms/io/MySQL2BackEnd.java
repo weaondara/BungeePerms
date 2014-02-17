402c41360a372335f972739d83426f6c729cc9fc
weaondara <wea_ondara@alpenbock.net>
2014-02-17 14:26:20 +0100

    use db username not the requested username

--- src/main/java/net/alpenblock/bungeeperms/io/MySQL2BackEnd.java
+++ src/main/java/net/alpenblock/bungeeperms/io/MySQL2BackEnd.java
@@ -189,7 +189,7 @@ public class MySQL2BackEnd implements BackEnd
                 }
             }
 			
-			Group group=new Group(g, inheritances, globalperms, servers, rank, ladder, isdefault, display, prefix, suffix);
+			Group group=new Group(mpe.getName(), inheritances, globalperms, servers, rank, ladder, isdefault, display, prefix, suffix);
 			ret.add(group);
 		}
         Collections.sort(ret);
@@ -275,7 +275,7 @@ public class MySQL2BackEnd implements BackEnd
             }
         }
 
-        User u=new User(user, lgroups, globalperms, serverperms,serverworldperms);
+        User u=new User(mpe.getName(), lgroups, globalperms, serverperms,serverworldperms);
         return u;
     }
     @Override
