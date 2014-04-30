46ca1ffd3e23bbb18d41f9c1225afe29946ea950
weaondara <wea_ondara@alpenblock.net>
2014-04-30 16:22:49 +0200

    added uuid und playername fetch test

---
+++ src/test/java/net/alpenblock/bungeeperms/uuid/UUIDFetcherTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.alpenblock.bungeeperms.uuid;
+
+import java.util.UUID;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author alex
+ */
+public class UUIDFetcherTest
+{
+    
+    public UUIDFetcherTest()
+    {
+    }
+
+    @Test
+    public void testGetUUIDs()
+    {
+        UUID uuid=UUID.fromString("10976043-a820-4412-bfcd-bc7ec3e59998");
+        
+        UUID fetcheduuid = UUIDFetcher.getUUIDFromMojang("wea_ondara", null);
+        
+        assertTrue(uuid.equals(fetcheduuid));
+    }
+
+    @Test
+    public void testGetPlayerNames()
+    {
+        UUID uuid=UUID.fromString("10976043-a820-4412-bfcd-bc7ec3e59998");
+        
+        String name = UUIDFetcher.getPlayerNameFromMojang(uuid);
+        
+        assertTrue(name.equals("wea_ondara"));
+    }
+    
+}
