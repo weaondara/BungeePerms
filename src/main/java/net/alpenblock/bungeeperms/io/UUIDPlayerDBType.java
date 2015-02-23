ac1058f929eedbb5fa0338284ceb069083bc3abf
wea_ondara <wea_ondara@alpenblock.net>
2015-02-23 13:46:33 +0100

    added by name func

--- src/main/java/net/alpenblock/bungeeperms/io/UUIDPlayerDBType.java
+++ src/main/java/net/alpenblock/bungeeperms/io/UUIDPlayerDBType.java
@@ -1,8 +1,21 @@
 package net.alpenblock.bungeeperms.io;
 
-public enum UUIDPlayerDBType 
+public enum UUIDPlayerDBType
 {
+
     None,
     YAML,
     MySQL;
+
+    public static UUIDPlayerDBType getByName(String name)
+    {
+        for (UUIDPlayerDBType t : values())
+        {
+            if (t.name().equalsIgnoreCase(name))
+            {
+                return t;
+            }
+        }
+        return null;
+    }
 }
