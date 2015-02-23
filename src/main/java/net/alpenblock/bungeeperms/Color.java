c94474f43d59a4e3a6d1ad2e05cdc44aed234bd2
wea_ondara <wea_ondara@alpenblock.net>
2015-02-23 16:25:34 +0100

    cleanup

--- src/main/java/net/alpenblock/bungeeperms/Color.java
+++ src/main/java/net/alpenblock/bungeeperms/Color.java
@@ -2,45 +2,19 @@ package net.alpenblock.bungeeperms;
 
 import net.md_5.bungee.api.ChatColor;
 
-// TODO: Auto-generated Javadoc
-/**
- * The Class Color.
- */
-public class Color {
-	
-	/** The Text. */
-	public static ChatColor Text=ChatColor.GRAY;
-	
-	/** The Tool. */
-	public static ChatColor Tool=ChatColor.RED;
-	
-	/** The User. */
-	public static ChatColor User=ChatColor.DARK_AQUA;
-	
-	/** The Success. */
-	public static ChatColor Success=ChatColor.GREEN;
-	
-	/** The Error. */
-	public static ChatColor Error=ChatColor.DARK_RED;
-	
-	/** The Activated. */
-	public static ChatColor Activated=ChatColor.GREEN;
-	
-	/** The Deactivated. */
-	public static ChatColor Deactivated=ChatColor.RED;
-	
-	/** The Value. */
-	public static ChatColor Value=ChatColor.GOLD;
-	
-	/** The Message. */
-	public static ChatColor Message=ChatColor.DARK_GREEN;
-	
-	/** The Severe. */
-	public static ChatColor Severe=ChatColor.RED;
-	
-	/** The Fatal. */
-	public static ChatColor Fatal=ChatColor.DARK_RED;
-	
-	/** The Link. */
-	public static String Link=ChatColor.DARK_BLUE+""+ChatColor.UNDERLINE;
+public class Color
+{
+
+    public static ChatColor Text = ChatColor.GRAY;
+    public static ChatColor Tool = ChatColor.RED;
+    public static ChatColor User = ChatColor.DARK_AQUA;
+    public static ChatColor Success = ChatColor.GREEN;
+    public static ChatColor Error = ChatColor.DARK_RED;
+    public static ChatColor Activated = ChatColor.GREEN;
+    public static ChatColor Deactivated = ChatColor.RED;
+    public static ChatColor Value = ChatColor.GOLD;
+    public static ChatColor Message = ChatColor.DARK_GREEN;
+    public static ChatColor Severe = ChatColor.RED;
+    public static ChatColor Fatal = ChatColor.DARK_RED;
+    public static String Link = ChatColor.DARK_BLUE + "" + ChatColor.UNDERLINE;
 }
