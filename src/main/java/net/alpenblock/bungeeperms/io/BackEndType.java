ab156cf7d66cfad02316cab7a033777821cbb030
wea_ondara <wea_ondara@alpenblock.net>
2015-02-23 13:32:11 +0100

    added by name func

--- src/main/java/net/alpenblock/bungeeperms/io/BackEndType.java
+++ src/main/java/net/alpenblock/bungeeperms/io/BackEndType.java
@@ -2,7 +2,20 @@ package net.alpenblock.bungeeperms.io;
 
 public enum BackEndType
 {
+
     YAML,
     MySQL,
-    MySQL2
+    MySQL2;
+
+    public static BackEndType getByName(String name)
+    {
+        for (BackEndType t : values())
+        {
+            if(t.name().equalsIgnoreCase(name))
+            {
+                return t;
+            }
+        }
+        return null;
+    }
 }
