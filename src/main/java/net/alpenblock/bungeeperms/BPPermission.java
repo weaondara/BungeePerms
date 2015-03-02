a79e333728c55d4394fbd5a58015cacd730fbe14
wea_ondara <wea_ondara@alpenblock.net>
2015-03-02 14:31:37 +0100

    cleanup

--- src/main/java/net/alpenblock/bungeeperms/BPPermission.java
+++ src/main/java/net/alpenblock/bungeeperms/BPPermission.java
@@ -1,59 +1,22 @@
 package net.alpenblock.bungeeperms;
 
-public class BPPermission 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class BPPermission
 {
+
     private String permission;
     private String origin;
     private boolean isGroup;
     private String server;
     private String world;
-
-    public BPPermission(String permission, String origin, boolean isGroup, String server, String world) {
-        this.permission = permission;
-        this.origin = origin;
-        this.isGroup = isGroup;
-        this.server = server;
-        this.world = world;
-    }
-
-    public String getPermission() {
-        return permission;
-    }
-
-    public void setPermission(String permission) {
-        this.permission = permission;
-    }
-
-    public String getOrigin() {
-        return origin;
-    }
-
-    public void setOrigin(String origin) {
-        this.origin = origin;
-    }
-
-    public boolean isGroup() {
-        return isGroup;
-    }
-
-    public void setGroup(boolean isGroup) {
-        this.isGroup = isGroup;
-    }
-
-    public String getServer() {
-        return server;
-    }
-
-    public void setServer(String server) {
-        this.server = server;
-    }
-
-    public String getWorld() {
-        return world;
-    }
-
-    public void setWorld(String world) {
-        this.world = world;
-    }
-    
 }
