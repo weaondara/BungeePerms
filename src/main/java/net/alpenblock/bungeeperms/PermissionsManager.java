8490722fc04da0be4ead76206baabe3a7374ee5b
wea_ondara <wea_ondara@alpenblock.net>
2015-03-08 00:36:17 +0100

    cleanup

--- src/main/java/net/alpenblock/bungeeperms/PermissionsManager.java
+++ src/main/java/net/alpenblock/bungeeperms/PermissionsManager.java
@@ -2,7 +2,6 @@ package net.alpenblock.bungeeperms;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
