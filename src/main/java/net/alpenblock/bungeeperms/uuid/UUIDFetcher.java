dfe694c058c7241f28fc373c270d52cf12435238
wea_ondara <wea_ondara@alpenblock.net>
2015-03-08 01:04:03 +0100

    formatting

--- src/main/java/net/alpenblock/bungeeperms/uuid/UUIDFetcher.java
+++ src/main/java/net/alpenblock/bungeeperms/uuid/UUIDFetcher.java
@@ -17,100 +17,105 @@ import net.alpenblock.bungeeperms.Statics;
 
 public class UUIDFetcher
 {
+
     private HttpProfileRepository repo;
-    
+
     @Getter
     private final List<String> tofetch;
     @Getter
-    private final Map<String,UUID> UUIDs;
+    private final Map<String, UUID> UUIDs;
     @Getter
-    private final Map<UUID,String> playerNames;
-    
+    private final Map<UUID, String> playerNames;
+
     private int cooldown;
 
     public UUIDFetcher(List<String> tofetch, int cooldown)
     {
-        repo=new HttpProfileRepository();
-        
+        repo = new HttpProfileRepository();
+
         this.tofetch = tofetch;
-        UUIDs=new HashMap<>();
-        playerNames=new HashMap<>();
-        this.cooldown=cooldown;
+        UUIDs = new HashMap<>();
+        playerNames = new HashMap<>();
+        this.cooldown = cooldown;
     }
 
     @SneakyThrows
     public void fetchUUIDs()
     {
-        for(String player:tofetch)
+        for (String player : tofetch)
         {
             UUID uuid = getUUIDFromMojang(player, repo);
-            if(uuid!=null)
+            if (uuid != null)
             {
                 UUIDs.put(player, uuid);
             }
             Thread.sleep(cooldown);
         }
     }
+
     @SneakyThrows
     public void fetchPlayerNames()
     {
-        for(String suuid:tofetch)
+        for (String suuid : tofetch)
         {
-            UUID uuid=UUID.fromString(suuid);
+            UUID uuid = UUID.fromString(suuid);
             String playername = getPlayerNameFromMojang(uuid);
-            if(playername!=null)
+            if (playername != null)
             {
                 playerNames.put(uuid, playername);
             }
             Thread.sleep(cooldown);
         }
     }
-    
+
     public static UUID getUUIDFromMojang(String player, HttpProfileRepository repo)
     {
-        if(repo==null)
+        if (repo == null)
         {
-            repo=new HttpProfileRepository();
+            repo = new HttpProfileRepository();
         }
-        
+
         Profile[] profiles = repo.findProfilesByCriteria(new ProfileCriteria(player, "minecraft"));
-        for(Profile p:profiles)
+        for (Profile p : profiles)
         {
-            UUID uuid=Statics.parseUUID(p.getId());
-            if(uuid!=null)
+            UUID uuid = Statics.parseUUID(p.getId());
+            if (uuid != null)
             {
                 return uuid;
             }
         }
-        
+
         return null;
     }
+
     public static String getPlayerNameFromMojang(UUID uuid)
     {
-        String suuid=uuid.toString().toLowerCase().replaceAll("-", "");
-        
+        String suuid = uuid.toString().toLowerCase().replaceAll("-", "");
+
         try
         {
-            URL url=new URL("https://sessionserver.mojang.com/session/minecraft/profile/"+suuid);
+            URL url = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + suuid);
             URLConnection con = url.openConnection();
             InputStream in = con.getInputStream();
-            byte[] buffer=new byte[con.getContentLength()];
+            byte[] buffer = new byte[con.getContentLength()];
             in.read(buffer);
-            String res=new String(buffer);
-            Gson gson=new Gson();
+            String res = new String(buffer);
+            Gson gson = new Gson();
             PlayerNameFetchResult result = gson.fromJson(res, PlayerNameFetchResult.class);
             return result.getName();
         }
-        catch(Exception e)
+        catch (Exception e)
         {
             e.printStackTrace();
         }
         return null;
     }
-    
-    @Getter @Setter
+
+    @Getter
+    @Setter
     private class PlayerNameFetchResult
     {
+
         private String id;
         private String name;
     }
