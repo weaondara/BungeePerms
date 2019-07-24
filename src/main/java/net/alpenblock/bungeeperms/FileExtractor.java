a9ae1fd323138d963f20d037ba4ddc918c4be700
wea_ondara <wea_ondara@alpenblock.net>
2019-07-24 23:10:29 +0200

    also extract bulgarian lang file

--- src/main/java/net/alpenblock/bungeeperms/FileExtractor.java
+++ src/main/java/net/alpenblock/bungeeperms/FileExtractor.java
@@ -22,6 +22,7 @@ public class FileExtractor
         }
         ALL_FILES.put("lang/EN-gb.yml", BungeePerms.getInstance().getPlugin().getPluginFolderPath() + "/lang/EN-gb.yml");
         ALL_FILES.put("lang/DE-de.yml", BungeePerms.getInstance().getPlugin().getPluginFolderPath() + "/lang/DE-de.yml");
+        ALL_FILES.put("lang/BG-de.yml", BungeePerms.getInstance().getPlugin().getPluginFolderPath() + "/lang/BG-de.yml");
     }
 
     public static void extractAll()
