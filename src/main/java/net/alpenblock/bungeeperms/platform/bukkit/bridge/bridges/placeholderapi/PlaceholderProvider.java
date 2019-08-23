10e522488c745a9cc66a2af2019db5b2bc3fda96
Aurora <aurora@relanet.eu>
2019-08-23 15:55:17 +0200

    impl placeholder api

--- src/main/java/net/alpenblock/bungeeperms/platform/bukkit/bridge/bridges/placeholderapi/PlaceholderProvider.java
+++ src/main/java/net/alpenblock/bungeeperms/platform/bukkit/bridge/bridges/placeholderapi/PlaceholderProvider.java
@@ -1,10 +1,17 @@
 package net.alpenblock.bungeeperms.platform.bukkit.bridge.bridges.placeholderapi;
 
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import net.alpenblock.bungeeperms.BungeePerms;
+import net.alpenblock.bungeeperms.BungeePermsAPI;
+import net.alpenblock.bungeeperms.Group;
+import net.alpenblock.bungeeperms.PermissionsManager;
 import org.bukkit.entity.Player;
 
 public class PlaceholderProvider extends PlaceholderExpansion {
+    
+    private PermissionsManager p = BungeePerms.getInstance().getPermissionsManager();
 
     @Override
     public boolean persist(){
@@ -34,22 +41,42 @@ public class PlaceholderProvider extends PlaceholderExpansion {
     @Override
     public String onPlaceholderRequest(Player player, String identifier){
 
-//        if(player == null){
-//            return "";
-//        }
-//
-//        // %someplugin_placeholder1%
-//        if(identifier.equals("placeholder1")){
-//            return plugin.getConfig().getString("placeholder1", "value doesnt exist");
-//        }
-//
-//        // %someplugin_placeholder2%
-//        if(identifier.equals("placeholder2")){
-//            return plugin.getConfig().getString("placeholder2", "value doesnt exist");
-//        }
-// 
-//        // We return null if an invalid placeholder (f.e. %someplugin_placeholder3%) 
-//        // was provided
+        if(player == null){
+            return "";
+        }
+
+        if(identifier.equals("groups")){
+            return BungeePermsAPI.userAllGroups(player.getName()).stream().collect(Collectors.joining(", "));
+        }
+        
+        if(identifier.equals("primary_group_name")){
+            return BungeePermsAPI.userMainGroup(player.getName());
+        }
+        
+        if(identifier.equals("prefix")){
+            return BungeePermsAPI.userPrefix(player.getName(), "", "");
+        }
+        
+        if(identifier.equals("prefix_primary_group")){
+            return p.getMainGroup(p.getUser(player.getName())).getPrefix();
+        }
+        
+        if(identifier.equals("suffix")){
+            return BungeePermsAPI.userSuffix(player.getName(), "", "");
+        }
+        
+        if(identifier.equals("suffix_primary_group")){
+            return p.getMainGroup(p.getUser(player.getName())).getSuffix();
+        }
+        
+        if(identifier.startsWith("in_group_")){       
+            return "" + BungeePermsAPI.userInGroup(player.getName(), identifier.substring("in_group_".length()));
+        }
+        
+        if(identifier.startsWith("has_permission_")){       
+            return "" + BungeePermsAPI.userHasPermission(player.getName(), identifier.substring("has_permission_".length()), "", "");
+        }
+        
         return null;
     }
 }
